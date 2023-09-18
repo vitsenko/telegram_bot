c394edc742c5d33d915fb6153e067e56d9adaa9c
vitsenko <127158860+vitsenko@users.noreply.github.com>
2023-09-18 20:11:31 +0300

    Додано відправку повідомлень за розкладом

--- main.py
+++ main.py
@@ -8,27 +8,36 @@ import os
 from datetime import datetime, timedelta
 from background import keep_alive
 import time
-import sched
 
 # Визначення глобальної змінної karma_regex (пошук слова Карма+- без врахування регістру)
 karma_regex = re.compile(r'.*(карма\s*[+\-]).*', re.IGNORECASE)
-# Список файлів з хендлерами
+
+# Список файлів з хендлерами та іншими функціями
 import handler_karma_status
 import handler_karma_handler
+from sender_warloses import daily_scheduler_run
 
 bot = telebot.TeleBot(os.environ['bot_token'])
+
 
 # Обробник команди karma_status. Відправляю дашборд по кармі в групу
 @bot.message_handler(commands=['karma_status'])
 def karma_status(message):
     handler_karma_status.karma_status(message)
-    
+
+
+# Обробник додавання та віднімання карми (карма+ / карма-)    
 @bot.message_handler(func=lambda message: message.chat.type == 'group' and karma_regex.search(message.text))
 def karma_handler(message):
     handler_karma_handler.karma_handler(message)
-    
-# Запускаєм flask сервер
+
+
+
+# Запускаєм flask сервер (веб-сервіс, який пінгується монітором, щоб дозволяє звлишити replit активним)
 keep_alive()
+
+# Запускаєм відправку повідомлень за розкладом
+daily_scheduler_run()
 
 # Запускаємо бота
 bot.infinity_polling()
